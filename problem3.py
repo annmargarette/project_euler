2d4db5b06069748d7f41c23f1dce5e7e199d78f9
annmargarette <ann.margarette.concepcion@gmail.com>
2025-12-23 20:17:14 +0800

    problem 3

---
+++ problem3.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.stdin = open("input.txt","r")
+sys.stdout = open("output.txt","w")
+
+n = 600851475143
+factor = []
+
+i = 2
+
+while(i*i < n):
+    if n%i==0:
+        while n%i == 0:
+            factor.append(i)
+            n/=i
+    i+=1
+
+if n>1:
+    factor.append(n)
+
+print(max(factor))
+    
